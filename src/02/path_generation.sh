110e289e736d0b591979931c0a8d92fcd084527f
kristieg <earsy@earsy>
2023-12-31 19:39:50 +0300

    2_part

--- src/02/path_generation.sh
+++ src/02/path_generation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PathGen() {
-  pwd=$(find / -type d \( ! -path "*bin*" -a ! -path "*sbin*" \) 2>/dev/null | shuf -n 1)
+  pwd=$(find / -type d -writable \( ! -path "*bin*" -a ! -path "*sbin*" \) 2>/dev/null | shuf -n 1)
   folder=($(FolderGen $1))
   path="${pwd%/}/${folder%/}"
   echo $path
--- src/02/touch.sh
+++ src/02/touch.sh
@@ -16,7 +16,7 @@ Touch() {
       touch_path="${folder_name%/}/${file_name}"
       time=$(date +'%Y-%m-%d %H:%M:%S')
       file_size=${file_size_full%%Mb*}
-      dd if=/dev/zero of="$touch_path" bs=1M count="$file_size" status=none
+      fallocate $touch_path -l $file_sizeMB
       echo path: $touch_path time: $time file_size: $file_size_full >> "log.txt"
     done
   done
