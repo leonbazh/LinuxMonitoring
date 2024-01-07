9918912c63fd5310f4f6e251bca074e0b58b7d99
kristieg <earsy@earsy>
2024-01-07 04:57:11 +0300

    9_part

--- src/09/main.sh
+++ src/09/main.sh
@@ -5,18 +5,20 @@ output_file="/etc/nginx/metrics/metrics.html"
 while true
 do
     cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-    memory_usage=$(free | awk '/Mem/ { printf("%.2f"), $3/$2 * 100 }')
-    memory_free=$(free | awk '/Mem/ {print $2}')
+    memory_total=$(free | awk '/Mem/ {print $2}')
+    memory_usage=$(free | awk '/Mem/ { printf $2 - $ 4 }')
+    memory_free=$(free | awk '/Mem/ {print $4}')
     disk_total=$(df / | awk 'NR==2 {print $2}')
     disk_usage=$(df / | awk 'NR==2 {print $3}')
     disk_free=$(df / | awk 'NR==2 {print $4}')
     
-    echo "system_cpu_usage $cpu_usage
-    system_memory_usage $memory_usage
-    system_memory_free $memory_free
-    system_disk_usage $disk_usage
-    system_disk_free $disk_free
-    system_disk_total $disk_total" > $output_file
+    echo system_cpu_usage $cpu_usage > $output_file
+    echo system_memory_total $memory_total >> $output_file
+    echo system_memory_usage $memory_usage >> $output_file
+    echo system_memory_free $memory_free >> $output_file
+    echo system_disk_usage $disk_usage >> $output_file
+    echo system_disk_free $disk_free >> $output_file
+    echo system_disk_total $disk_total >> $output_file
 
     sleep 3
 done
