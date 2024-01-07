e078a2699d4e16ea68e55ad4389f758868369f59
kristieg <earsy@earsy>
2024-01-07 04:47:48 +0300

    9_part

--- src/09/main.sh
+++ src/09/main.sh
@@ -6,18 +6,17 @@ while true
 do
     cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
     memory_usage=$(free | awk '/Mem/ { printf("%.2f"), $3/$2 * 100 }')
-    disk_usage=$(df -h / | awk 'NR==2 {print $5}')
+    memory_free=$(free | awk '/Mem/ {print $2}')
+    disk_total=$(df / | awk 'NR==2 {print $2}')
+    disk_usage=$(df / | awk 'NR==2 {print $3}')
+    disk_free=$(df / | awk 'NR==2 {print $4}')
     
-    timestamp=$(date +%s)
-    
-    cat <<EOF >$output_file
-# TYPE system_cpu_usage gauge
-system_cpu_usage{instance="localhost"} $cpu_usage $timestamp
-# TYPE system_memory_usage gauge
-system_memory_usage{instance="localhost"} $memory_usage $timestamp
-# TYPE system_disk_usage gauge
-system_disk_usage{instance="localhost"} $disk_usage $timestamp
-EOF
+    echo "system_cpu_usage $cpu_usage
+    system_memory_usage $memory_usage
+    system_memory_free $memory_free
+    system_disk_usage $disk_usage
+    system_disk_free $disk_free
+    system_disk_total $disk_total" > "metrics.html"
 
     sleep 3
 done
